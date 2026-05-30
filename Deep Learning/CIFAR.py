c600dbc5d29af25a4f2c91d33e33154c503f916d
Rohit123iitan <rohitkumar17154849@gmail.com>
2026-05-30 22:02:57 +0530

    cifar

--- Deep Learning/CIFAR.py
+++ Deep Learning/CIFAR.py
@@ -34,3 +34,132 @@ img, label = dataset[0]
 print(img.shape, label)
 img
 print(dataset.classes)
+
+random_seed = 42
+torch.manual_seed(random_seed)
+
+val_size = 5000
+train_size = len(dataset) - val_size
+
+train_ds, val_ds = random_split(dataset, [train_size, val_size])
+len(train_ds), len(val_ds)
+
+from torch.utils.data.dataloader import DataLoader
+
+batch_size=128
+train_dl = DataLoader(train_ds, batch_size, shuffle=True, num_workers=4, pin_memory=True)
+val_dl = DataLoader(val_ds, batch_size*2, num_workers=4, pin_memory=True)
+
+from torchvision.utils import make_grid
+
+def show_batch(dl):
+    for images, labels in dl:
+        fig, ax = plt.subplots(figsize=(12, 6))
+        ax.set_xticks([]); ax.set_yticks([])
+        ax.imshow(make_grid(images, nrow=16).permute(1, 2, 0))
+        break
+    
+show_batch(train_dl)
+
+def apply_kernel(image, kernel):
+    ri, ci = image.shape       # image dimensions
+    rk, ck = kernel.shape      # kernel dimensions
+    ro, co = ri-rk+1, ci-ck+1  # output dimensions
+    output = torch.zeros([ro, co])
+    for i in range(ro): 
+        for j in range(co):
+            output[i,j] = torch.sum(image[i:i+rk,j:j+ck] * kernel)
+    return output
+
+sample_image = torch.tensor([
+    [3, 3, 2, 1, 0], 
+    [0, 0, 1, 3, 1], 
+    [3, 1, 2, 2, 3], 
+    [2, 0, 0, 2, 2], 
+    [2, 0, 0, 0, 1]
+], dtype=torch.float32)
+
+sample_kernel = torch.tensor([
+    [0, 1, 2], 
+    [2, 2, 0], 
+    [0, 1, 2]
+], dtype=torch.float32)
+
+apply_kernel(sample_image, sample_kernel)
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+simple_model = nn.Sequential(
+    nn.Conv2d(3, 8, kernel_size=3, stride=1, padding=1),
+    nn.MaxPool2d(2, 2)
+)
+
+for images, labels in train_dl:
+    print('images.shape:', images.shape)
+    out = simple_model(images)
+    print('out.shape:', out.shape)
+    break
+
+class ImageClassificationBase(nn.Module):
+    def training_step(self, batch):
+        images, labels = batch 
+        out = self(images)                  # Generate predictions
+        loss = F.cross_entropy(out, labels) # Calculate loss
+        return loss
+    
+    def validation_step(self, batch):
+        images, labels = batch 
+        out = self(images)                    # Generate predictions
+        loss = F.cross_entropy(out, labels)   # Calculate loss
+        acc = accuracy(out, labels)           # Calculate accuracy
+        return {'val_loss': loss.detach(), 'val_acc': acc}
+        
+    def validation_epoch_end(self, outputs):
+        batch_losses = [x['val_loss'] for x in outputs]
+        epoch_loss = torch.stack(batch_losses).mean()   # Combine losses
+        batch_accs = [x['val_acc'] for x in outputs]
+        epoch_acc = torch.stack(batch_accs).mean()      # Combine accuracies
+        return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
+    
+    def epoch_end(self, epoch, result):
+        print("Epoch [{}], train_loss: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}".format(
+            epoch, result['train_loss'], result['val_loss'], result['val_acc']))
+        
+def accuracy(outputs, labels):
+    _, preds = torch.max(outputs, dim=1)
+    return torch.tensor(torch.sum(preds == labels).item() / len(preds))
+
+class Cifar10CnnModel(ImageClassificationBase):
+    def __init__(self):
+        super().__init__()
+        self.network = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2), # output: 64 x 16 x 16
+
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2), # output: 128 x 8 x 8
+
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2), # output: 256 x 4 x 4
+
+            nn.Flatten(), 
+            nn.Linear(256*4*4, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10))
+        
+    def forward(self, xb):
+        return self.network(xb)
+model = Cifar10CnnModel()
+model
