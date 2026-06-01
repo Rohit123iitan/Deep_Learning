c765cee20e569f17685dc22de533e476419dae6a
Rohit123iitan <rohitkumar17154849@gmail.com>
2026-06-01 22:02:26 +0530

    cifar

--- Deep Learning/CIFAR.py
+++ Deep Learning/CIFAR.py
@@ -163,3 +163,38 @@ class Cifar10CnnModel(ImageClassificationBase):
         return self.network(xb)
 model = Cifar10CnnModel()
 model
+
+for images, labels in train_dl:
+    print('images.shape:', images.shape)
+    out = model(images)
+    print('out.shape:', out.shape)
+    print('out[0]:', out[0])
+    break
+
+def get_default_device():
+    """Pick GPU if available, else CPU"""
+    if torch.cuda.is_available():
+        return torch.device('cuda')
+    else:
+        return torch.device('cpu')
+    
+def to_device(data, device):
+    """Move tensor(s) to chosen device"""
+    if isinstance(data, (list,tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device, non_blocking=True)
+
+class DeviceDataLoader():
+    """Wrap a dataloader to move data to a device"""
+    def __init__(self, dl, device):
+        self.dl = dl
+        self.device = device
+        
+    def __iter__(self):
+        """Yield a batch of data after moving it to device"""
+        for b in self.dl: 
+            yield to_device(b, self.device)
+
+    def __len__(self):
+        """Number of batches"""
+        return len(self.dl)
